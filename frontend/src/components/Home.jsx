29980daa8bc0b2fb58a0de0f50ef405ca24ed70d
andremelc <andre.melendezc@unmsm.edu.pe>
2025-02-17 00:20:38 -0500

    Profile card almost finished

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../styles/Home.css";
+import imagenEjemplo from "../assets/imagen_ejemplo.jpg";
 
 const Home = () => {
   return (
     <section className="home-section">
+      <div className="home-card">
+        <img src={imagenEjemplo} alt="Profile" className="profile-image" />
+        <h1 className="h1-card">Andre Melendez Cava</h1>
+        <h2 className="h2-card">Full Stack Developer</h2>
+      </div>
     </section>
   );
 };
