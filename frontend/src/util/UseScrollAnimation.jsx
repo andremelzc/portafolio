48e2b1100ff286b989384e6c7a5ec5d97a1e886e
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-03-31 22:45:29 -0500

    Trying v11

---
+++ frontend/src/util/UseScrollAnimation.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from "react";
+
+const UseScrollAnimation = (threshold = 0.5, delay = 300) => { // ss
+  const ref = useRef(null);
+  const [visible, setVisible] = useState(false);
+  let timeoutId = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        clearTimeout(timeoutId.current);
+        if (entry.isIntersecting) {
+          setVisible(true);
+        } else {
+          timeoutId.current = setTimeout(() => setVisible(false), delay);
+        }
+      },
+      { threshold }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      clearTimeout(timeoutId.current);
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, [threshold, delay]);
+
+  return { ref, visible };
+};
+
+export default UseScrollAnimation;
