8ee64780bd95b6af62cf952fc966083969ec183f
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-03-31 22:22:59 -0500

    Trying v6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist', // Esta es la clave - salida un nivel arriba de frontend
-    emptyOutDir: true, // Limpia el directorio de salida antes de construir
-  },
 })
